5e8c21a141b195ab0795a0a1d6af9debf2397acf
8309h <harshalwagh201718@gmail.com>
2022-11-10 23:51:52 +0530

    day-3 work

---
+++ script/women.js
@@ -0,0 +1,422 @@
+let women =[
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71fRSdThqxL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.598"
+    },
+    {
+      "Title": "The Monkey Castle",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/31M-kCxCteL._AC_UL320_.jpg",
+      "Description": "T-Shirt Black Round Neck Pure Cotton for Women Regular Fit",
+      "Price": "Rs.1,299"
+    },
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/815TSD8oPvL._AC_UL320_.jpg",
+      "Description": "Women's Regular T-Shirt",
+      "Price": "Rs.798"
+    },
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81Y46IeIwRL._AC_UL320_.jpg",
+      "Description": "Women's Regular T-Shirt",
+      "Price": "Rs.699"
+    },
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71D0C3+Gs8L._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.798"
+    },
+    {
+      "Title": "Fabricorn",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61hbqL0V5JL._AC_UL320_.jpg",
+      "Description": "Stylish Plain Up and Down Cotton Tshirt for Women",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "SHAUN",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71qBmgnJJKL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt (N704WT3_$P_Pack of 3)",
+      "Price": "Rs.999"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71pddDdFKaL._AC_UL320_.jpg",
+      "Description": "Cotton Blend Half Sleeve Boyfriend/Loose Fit T-Shirts for Women",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61i7Nh3iuWL._AC_UL320_.jpg",
+      "Description": "Oversized Cotton Blend Half Sleeve Stronger Printed Womens T-Shirts",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Fabricorn",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71bxu0tQh1L._AC_UL320_.jpg",
+      "Description": "Stylish Plain Green Long Sleeve Cotton Tshirt for Women (Green)",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71AsTN-u3+L._AC_UL320_.jpg",
+      "Description": "Women's Solid Regular Fit Half Sleeve T-Shirt (Combo Pack of 3)",
+      "Price": "Rs.897"
+    },
+    {
+      "Title": "SHRIEZ",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/41RbMoK2dCL._AC_UL320_.jpg",
+      "Description": "Oversized T-Shirt for Women, Printed Half Sleeve T-Shirt for Women/Girls",
+      "Price": "Rs.625"
+    },
+    {
+      "Title": "Maniac",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71R3ypx-OEL._AC_UL320_.jpg",
+      "Description": "Women's Solid Hooded Neck Fulleleeve Lilac Color Cotton Slim Fit Crop Tshirt",
+      "Price": "Rs.1,399"
+    },
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71rPSIhyJ5L._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,598"
+    },
+    {
+      "Title": "OM SAI LATEST CREATION",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/6109c+c00qL._AC_UL320_.jpg",
+      "Description": "Women's Lycra Striped Tie-Dye T-Shirt for Women Western Tees Travel, Casual, Night Wear Western Tops",
+      "Price": "Rs.1,999"
+    },
+    {
+      "Title": "FUNDAY FASHION",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61cxD6AY-dL._AC_UL320_.jpg",
+      "Description": "Cotton Half Sleeve Printed Oversized T-Shirt for Women/Girls",
+      "Price": "Rs.1,499"
+    },
+    {
+      "Title": "Generic",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61+3zUw8cML._AC_UL320_.jpg",
+      "Description": "Women's Full Sleeve Cotton T-Shirt(Sun Flower)",
+      "Price": "Rs.899"
+    },
+    {
+      "Title": "Levi's",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71wFSQmyLdL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Harpa Basics",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61hQAO5R9-L._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.999"
+    },
+    {
+      "Title": "DHRUVI TRENDZ",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81zze60t5kL._AC_UL320_.jpg",
+      "Description": "Women's Lycra Striped Tie-Dye T-Shirt for Women Western Tees Travel, Casual, Night Wear Western Tops",
+      "Price": "Rs.1,999"
+    },
+    {
+      "Title": "Ap'pulse",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61eb5QQFvmL._AC_UL320_.jpg",
+      "Description": "Women's Long Sleeve Thumbopen Hoodie",
+      "Price": "Rs.1,499"
+    },
+    {
+      "Title": "Stories.Label",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61QDgoj4UyL._AC_UL320_.jpg",
+      "Description": "Women Printed Cotton T-Shirt in Regular Fit and Half Sleeves, Casual Tops for Girls",
+      "Price": "Rs.1,299"
+    },
+    {
+      "Title": "Ap'pulse",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71IWYzZfUqL._AC_UL320_.jpg",
+      "Description": "Women's T-Shirt (Pack of 2)",
+      "Price": "Rs.1,058"
+    },
+    {
+      "Title": "PLAGG",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61L4LBm2jZL._AC_UL320_.jpg",
+      "Description": "Women Cotton Round Neck Full Sleeve Regular Fit Tshirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "JUNEBERRY",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61K1AMOSdFL._AC_UL320_.jpg",
+      "Description": "Cotton Regular Fit Printed Half Sleeves T-Shirt for Women",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "JUNEBERRY",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/519XEWOmXHL._AC_UL320_.jpg",
+      "Description": "Women's Regular Fit T-Shirt",
+      "Price": "Rs.1,194"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61gbhfCSB8L._AC_UL320_.jpg",
+      "Description": "Oversized Cotton Blend Half Sleeve Womens T-Shirts",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "JUNEBERRY",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/51ndqbrM9QL._AC_UL320_.jpg",
+      "Description": "Cotton Blend Tie-Dye Round Neck Half Sleeve Regular Fit Printed T-Shirt for Women & Girls (JB_TSH_TD_Babe)",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Max",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71RhajM-n6L._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,194"
+    },
+    {
+      "Title": "Van Heusen Athleisure",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61Xoy6tkeBL._AC_UL320_.jpg",
+      "Description": "Women's Regular fit T-Shirt",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Max",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81Vr4qPcelL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Amazon Brand - Myx",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81+dID22VmL._AC_UL320_.jpg",
+      "Description": "Women's Loose T-Shirt",
+      "Price": "Rs.899"
+    },
+    {
+      "Title": "Amazon Brand - Eden & Ivy",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71VhlH7DVuL._AC_UL320_.jpg",
+      "Description": "Women's Slim T-Shirt (ENISS22WTE509_Jet Black 4XL)",
+      "Price": "Rs.999"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71a8XDmBJbL._AC_UL320_.jpg",
+      "Description": "Oversized Cotton Blend Half Sleeve Womens T-Shirts",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Adidas",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71vZjynL6uL._AC_UL320_.jpg",
+      "Description": "Women's Regular T-Shirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "JUNEBERRY",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71ccR6sELML._AC_UL320_.jpg",
+      "Description": "Cotton Half Sleeve Printed T-Shirt for Women, Women's Round Neck Half Sleeves Gym Sports T-Shirt, Casual Women's Regular T-Shirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Amazon Brand - Symbol",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71cNbQmSBEL._AC_UL320_.jpg",
+      "Description": "Women's Regular T-Shirt",
+      "Price": "Rs.598"
+    },
+    {
+      "Title": "JUNEBERRY",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61Y5NQMuokL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Adidas",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71pBLY91QnL._AC_UL320_.jpg",
+      "Description": "Women's Fitted T-Shirts",
+      "Price": "Rs.1,299"
+    },
+    {
+      "Title": "Allen Solly",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/51FqcY3dvZS._AC_UL320_.jpg",
+      "Description": "Women's Regular T-Shirt",
+      "Price": "Rs.649"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61OKluwpxFL._AC_UL320_.jpg",
+      "Description": "Oversize Cottonblend Half Sleeve T-Shirts for Women",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81bT3RPxQbL._AC_UL320_.jpg",
+      "Description": "Oversized Cotton Blend Half Sleeve Womens T-Shirts Combo (Pack of 3)",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Veirdo",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61NSV7BH1IL._AC_UL320_.jpg",
+      "Description": "Oversized Tshirt for Women, Half Sleeves Printed T Shirt for Women & Girls, Ladies Oversized T Shirts, Loose Fit Baggy Tshirt for Women, Drop Shoulder Tshirt for Women",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Park Avenue Women",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61t7o3WMqDL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,799"
+    },
+    {
+      "Title": "Amazon Brand - Eden & Ivy",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81x+7ghyIgL._AC_UL320_.jpg",
+      "Description": "Women Work Utility T-Shirt",
+      "Price": "Rs.598"
+    },
+    {
+      "Title": "Levi's",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81J4dVFZgVL._AC_UL320_.jpg",
+      "Description": "Women's Regular Fit T-Shirt",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Adidas",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71vZjynL6uL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71foOIdlxcL._AC_UL320_.jpg",
+      "Description": "Regular Fit Cotton-Blend Half Sleeve Men's T-Shirt",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/61UZ2csaAaL._AC_UL320_.jpg",
+      "Description": "Cotton Blend Half Sleeve Dragon Printed Oversized T-Shirts for Men",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Veirdo",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/615cjFH2YAL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,100"
+    },
+    {
+      "Title": "Max",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81zTkFr2VLL._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,101"
+    },
+    {
+      "Title": "JUNEBERRY",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/51RP5CKYdoL._AC_UL320_.jpg",
+      "Description": "Tie Dye Regular Fit Half Sleeves T-Shirts for Women/Girls",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Levi's",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71j+q-KTKML._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Levi's",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71YMh6i7xML._AC_UL320_.jpg",
+      "Description": "Women T-Shirt",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Van Heusen Athleisure",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71AmqeJ2+dL._AC_UL320_.jpg",
+      "Description": "Van Heusen Women T-Shirt - Anti Bacterial, Round Neck, Quick Dry",
+      "Price": "Rs.1,100"
+    },
+    {
+      "Title": "LEOTUDE",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/71DctqqzLxL._AC_UL320_.jpg",
+      "Description": "Cotton Blend Printed Half Sleeve Oversized T-Shirts for Women",
+      "Price": "Rs.1,099"
+    },
+    {
+      "Title": "Fabricorn",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/716Um6A9OtL._AC_UL320_.jpg",
+      "Description": "Combo of Two Women's Cotton V-Neck and Round Neck Up Down Short Sleeve Tshirt",
+      "Price": "Rs.1,199"
+    },
+    {
+      "Title": "Enamor",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/41YMx+KBYdL._AC_UL320_.jpg",
+      "Description": "Women's Slim Fit T-Shirt",
+      "Price": "Rs.1,200"
+    },
+    {
+      "Title": "Enamor",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/41zd9SNYGOL._AC_UL320_.jpg",
+      "Description": "Essentials Full Sleeve Round Neck Slim Fit Stretch Cotton Lounge Tshirt for Womens-E057",
+      "Price": "Rs.1,201"
+    },
+    {
+      "Title": "Ap'pulse",
+      "Catogory": "women's clothing",
+      "Image": "https://m.media-amazon.com/images/I/81OeOFjSxwL._AC_UL320_.jpg",
+      "Description": "Women's Longsleeve Round Neck(Pack of 3)",
+      "Price": "Rs.1,587"
+    }
+   ]
